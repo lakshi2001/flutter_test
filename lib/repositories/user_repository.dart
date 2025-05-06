d7cf9b018b1892b75ec8a9d1aba40ce78eb80598
lakshika_dev <lakshika.jayasinghe@elegantmedia.lk>
2025-05-06 13:52:08 +0530

    ⚡ create UserRepository to handle API requests

--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -1,21 +1,21 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/user.dart';
 
-// TODO: Implement UserRepository
-// Requirements:
-// - Extract data fetching logic from UserProfileScreen
-// - Implement a method to fetch user by ID
-// - Return a User object instead of Map<String, dynamic>
-// - Handle errors properly
-
 class UserRepository {
   final http.Client _client;
-  
+
   UserRepository({http.Client? client}) : _client = client ?? http.Client();
-  
-  // TODO: Implement getUser method
+
   Future<User> getUser(String userId) async {
-    // TODO: Implement API call to fetch user data
-    throw UnimplementedError('getUser has not been implemented');
+    final url = Uri.parse('https://jsonplaceholder.typicode.com/users/$userId');
+    final response = await _client.get(url);
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return User.fromJson(data);
+    } else {
+      throw Exception('Failed to load user data: ${response.statusCode}');
+    }
   }
 }
