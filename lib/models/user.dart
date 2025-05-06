9f8b14ae9781f54ad22c98c752cb3ee8dcca35a8
lakshika_dev <lakshika.jayasinghe@elegantmedia.lk>
2025-05-06 13:51:31 +0530

    ⚡ add User model with fromJson constructor

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,17 +1,22 @@
-// TODO: Implement User model class
-// Requirements:
-// - Create a model class to represent user data
-// - Implement fromJson factory constructor
-// - Include properties for name, email, phone, website, etc.
-
 class User {
-  // TODO: Add properties
-  
-  // TODO: Add constructor parameters
-  
-  // TODO: Implement fromJson factory constructor
+  final String name;
+  final String email;
+  final String phone;
+  final String website;
+
+  User({
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.website,
+  });
+
   factory User.fromJson(Map<String, dynamic> json) {
-    // TODO: Parse JSON and return User instance
-    throw UnimplementedError('User.fromJson has not been implemented');
+    return User(
+      name: json['name'] ?? '',
+      email: json['email'] ?? '',
+      phone: json['phone'] ?? '',
+      website: json['website'] ?? '',
+    );
   }
 }
